551bffed96719e6a622b4cfe4fe121fab01384b5
AlejandroDiaz16 <sergiobustos69@gmail.com>
2024-11-09 16:48:59 -0500

    init

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import api from '../api'
-import TaskList from '../components/TaskList';
+import React from 'react'
 
 export default function Home() {
-    //variables
-    const [tasks, setTasks] = useState([]);
-
-    //functions
-    const getTasks = async () => {
-        const resp = await api('/tasks/getAllTasks', {method: 'GET'});
-        if (resp.error) {
-            console.log('there was an issue getting the tasks')
-        } else {
-            console.log('data here', resp)
-            setTasks(resp.data)
-        }
-    }
-
-    useEffect(() => {
-        getTasks();
-    }, [])
 
     return (
         <div>
-            <h1>Task management app</h1>
-            <TaskList tasks={tasks} onUpdate={getTasks}/>
+            Blueprint
         </div>
     )
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import currencyReducer from './currency';
-import languageReducer from './language';
+
 
 const store = configureStore({
     reducer: {
-        language: languageReducer,
-        currency: currencyReducer
     }
 });
 
